6b5e6f6e613f195f4aada956526cc89d1449f647
Oscar Gonzalez <oscarlidenbrock@gmail.com>
2024-09-09 21:39:09 +0000

    define ajax methods

--- ext/ajax.js
+++ ext/ajax.js
@@ -1,5 +1,13 @@
 jWeb.extend('ajax', {
-    test: function() {
-        alert('hola ajax!');
+    call: function(url, config, params, callback) {
+
+    },
+
+    get: function(url, params, callback) {
+        return jWeb.ajax.call(url, {method: 'GET'}, params, callback);
+    },
+
+    post: function(url, params, callback) {
+        return jWeb.ajax.call(url, {method: 'POST'}, params, callback);
     }
 });
