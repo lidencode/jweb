3c05e7b8c8993d209c3b9a513fcd457bc557421c
Oscar Gonzalez <oscarlidenbrock@gmail.com>
2024-09-24 18:48:06 +0000

    add dom.each() function

--- ext/dom.js
+++ ext/dom.js
@@ -1,4 +1,8 @@
 jWeb.extend('dom', {
+    each: function(query, callback) {
+        return document.querySelectorAll(query).forEach(callback);
+    },
+
     find: function(query) {
         return document.querySelectorAll(query);
     }
